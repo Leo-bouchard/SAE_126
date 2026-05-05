d20e34c22017403f32c91f86fe7d75ccd83e5c5d
bouchard <l64bouchard@gmail.com>
2026-05-05 13:10:42 +0200

    model

--- alquerque/model/Board.java
+++ alquerque/model/Board.java
@@ -1,6 +1,7 @@
 package alquerque.model;
 
 import boardifier.model.ContainerElement;
+import boardifier.model.GameElement;
 import boardifier.model.GameStageModel;
 
 import java.util.ArrayList;
@@ -18,11 +19,21 @@ public class Board extends ContainerElement {
         List<int[]> valid = new ArrayList<>();
         int[] pos = getElementCell(pawn);
         int x = pos[0];
-        int y = pos[1];
+        int y = pos[1];  // we look at the 8 box around the pawn
         for(int i=-1;i<=1;i++){
             for(int j = -1 ; j <= 1; j++){
-                if (x + i  >= 0 && x + i  < 5 && y + j  >= 0 && y + j  < 5 && !(i == 0 && j == 0) && ((x + y) % 2 == 0 || i == 0 || j == 0)){
-                    valid.add(new int[]{x + i  , y + j});
+                if (x + i  >= 0 && x + i  < 5 && y + j  >= 0 && y + j  < 5 && !(i == 0 && j == 0) && ((x + y) % 2 == 0 || i == 0 || j == 0 )&& isEmptyAt(x+i,y+j)) {
+
+                    //we add every empty cell around the pawn to a list
+                    valid.add(new int[]{x + i, y + j});
+
+                }
+                if (((Pawn) getElement(x+i, y+j)).getColor() != pawn.getColor()){
+                    if (x + i*2  >= 0 && x + i*2  < 5 && y + j*2  >= 0 && y + j*2  < 5 && isEmptyAt(x+i*2,y+j*2)){
+
+                        //we add every empty cell behind an enemy to the list
+                        valid.add(new int[]{x+i*2,y+j*2});
+                    }
                 }
             }
         }
