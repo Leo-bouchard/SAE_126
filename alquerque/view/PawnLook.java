6e47c790404b06dd4a021a381d6e01b7a2d5b4c0
bouchard <l64bouchard@gmail.com>
2026-05-05 15:42:38 +0200

    model

---
+++ alquerque/view/PawnLook.java
@@ -0,0 +1,25 @@
+package alquerque.view;
+
+import alquerque.model.Pawn;
+import boardifier.model.GameElement;
+import boardifier.view.ElementLook;
+
+import javax.swing.text.Element;
+
+public class PawnLook extends ElementLook {
+
+    public PawnLook(GameElement element){
+        super(element,1,1);
+    }
+
+    protected void render() {
+        Pawn pawn = (Pawn)element;
+        if (pawn.getColor()==0){
+            shape[0][0] = "●";
+        } else if(pawn.getColor()==0) {
+            shape[0][0] = "○";
+        }
+        }
+    }
+}
+
