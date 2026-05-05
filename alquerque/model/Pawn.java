93d5de928b61ba36e5eaca2e700b589f2f1c74f6
Emile <emile.cap@icloud.com>
2026-05-05 10:44:29 +0200

    ajout du pion

--- alquerque/model/Pawn.java
+++ alquerque/model/Pawn.java
@@ -1,14 +1,27 @@
 package alquerque.model;
 
+import boardifier.model.ElementTypes;
 import boardifier.model.GameElement;
+import boardifier.model.GameStageModel;
 
 public class Pawn extends GameElement {
 
-    int white;
 
-    public void Pawn(){
+    // white = 0    black = 1
+    int color;
+    int type;
 
+
+    public Pawn(int color, GameStageModel game) {
+        super(game);
+        // identification of the ElementType "pawn"
+        ElementTypes.register("Pawn", 19);
+        type = ElementTypes.getType("Pawn");
+        this.color = color;
     }
 
-    
+
+    public int getColor() {
+        return color;
+    }
 }
