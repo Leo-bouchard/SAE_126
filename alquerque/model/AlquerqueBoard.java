5d17267e79b0f36f25811ba8291bcf484742c036
Matsplash9 <telestoleneuvieme@gmail.com>
2026-05-21 16:04:33 +0200

    compute valid capture cells

--- alquerque/model/AlquerqueBoard.java
+++ alquerque/model/AlquerqueBoard.java
@@ -37,4 +37,22 @@ public class AlquerqueBoard extends ContainerElement {
             }
             return valid;
         }
+        public List<int[]> computeValidCaptureCells(AlquerquePawn pawn) {
+        List<int[]> validcapture = new ArrayList<>();
+        int[] pos = getElementCell(pawn);
+        int x = pos[0];
+        int y = pos[1];
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (x + i >= 0 && x + i < 5 && y + j >= 0 && y + j < 5 && !isEmptyAt(x + i, y + j) && ((AlquerquePawn) getElement(x + i, y + j)).getColor() != pawn.getColor()) {
+                    if (x + i * 2 >= 0 && x + i * 2 < 5 && y + j * 2 >= 0 && y + j * 2 < 5 && isEmptyAt(x + i * 2, y + j * 2)) {
+
+                        //we add every empty cell behind an enemy to the list
+                        validcapture.add(new int[]{x + i * 2, y + j * 2});
+                    }
+                }
+            }
+        }
+        return validcapture;
+    }
     }
